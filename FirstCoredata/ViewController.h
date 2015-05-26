0574ed5912154693611a88a3a1293e50e6da3404
Atul Gawali <atulgawali@Atuls-iMac.local>
2015-05-26 09:03:24 -0700

    Initial Commit

---
+++ FirstCoredata/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstCoredata
+//
+//  Created by Atul Gawali on 26/05/15.
+//  Copyright (c) 2015 Atul Gawali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FirstCoredata/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FirstCoredata
+//
+//  Created by Atul Gawali on 26/05/15.
+//  Copyright (c) 2015 Atul Gawali. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
